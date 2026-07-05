fb5bf3d4de567377052d5a81530182dfa0acbc38
陈虎明 <chenhuming@gmail.com>
2026-07-05 13:39:01 +0800

    Modify Package-Requires for compatibility
    Updated package requirements to support older Emacs versions and added additional dependencies.

--- gptel-agent-loop.el
+++ gptel-agent-loop.el
@@ -5,7 +5,7 @@
 ;; Author: Huming Chen <chenhuming@gmail.com>
 ;; URL: https://github.com/beacoder/gptel-agent-loop
 ;; Package-Version: 0.1
-;; Package-Requires: ((emacs "27.1") (gptel "0.9.9.5))
+;; Package-Requires: ((emacs "24.3") (compat "0.33.0") (nadvice "0.4") (gptel "0.9.9.5"))
 ;; Package-Author: Huming Chen
 ;; Package-Keywords: programming, convenience
 ;; Package-Description: Prevents gptel agent from stopping prematurely without confirming task completion
